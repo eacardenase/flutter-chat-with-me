d0fb5cae403ee5a6844770e85f62566dc27a1d77
Edwin Cardenas <eacardenase@unal.edu.co>
2024-02-23 08:23:21 -0500

    feat: upload chat messages to firestore

--- lib/widgets/new_message.dart
+++ lib/widgets/new_message.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 class NewMessage extends StatefulWidget {
   const NewMessage({super.key});
 
@@ -10,16 +13,29 @@ class NewMessage extends StatefulWidget {
 class _NewMessageState extends State<NewMessage> {
   final _messageController = TextEditingController();
 
-  void _submitMessage() {
+  void _submitMessage() async {
     final enteredMessage = _messageController.text;
 
     if (enteredMessage.trim().isEmpty) {
       return;
     }
 
-    // send to firebase
-
+    FocusScope.of(context).unfocus(); // hides the current shown keyboard
     _messageController.clear();
+
+    final user = FirebaseAuth.instance.currentUser!;
+    final userData = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(user.uid)
+        .get();
+
+    FirebaseFirestore.instance.collection('chat').add({
+      'text': enteredMessage,
+      'createdAt': Timestamp.now(),
+      'userId': user.uid,
+      'username': userData.data()!['username'],
+      'userImage': userData.data()!['image_url'],
+    });
   }
 
   @override
