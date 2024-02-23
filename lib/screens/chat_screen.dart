e271dbe40fb72e58ad4c98f4844fddc2cc792feb
Edwin Cardenas <eacardenase@unal.edu.co>
2024-02-23 12:07:32 -0500

    feat: add manual push notifications through firebase cloud messaging

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import 'package:chat_with_me/widgets/chat_messages.dart';
 import 'package:chat_with_me/widgets/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({
     super.key,
   });
 
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
   void _logout() async {
     await FirebaseAuth.instance.signOut();
+  }
+
+  void _setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+
+    await fcm.requestPermission();
+
+    final token = await fcm
+        .getToken(); // get current device address to use later for sending personalized notifications
+
+    print(token);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _setupPushNotifications();
   }
 
   @override
