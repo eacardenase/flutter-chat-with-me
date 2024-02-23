7fa063625ce2bd6b54de7b6c78ed780ce11e8a0b
Edwin Cardenas <eacardenase@unal.edu.co>
2024-02-23 14:11:16 -0500

    feat: add firebase cloud messaging for automatic push notifications

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -59,7 +59,7 @@ class _AuthScreenState extends State<AuthScreen> {
       });
 
       if (_isLogin) {
-        final userCredentials = await _firebase.signInWithEmailAndPassword(
+        await _firebase.signInWithEmailAndPassword(
           email: _enteredEmail,
           password: _enteredPassword,
         );
