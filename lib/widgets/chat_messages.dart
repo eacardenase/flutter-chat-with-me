f3dd7367246cd42b5ff4b6c1d0a3df9a7643975a
Edwin Cardenas <eacardenase@unal.edu.co>
2024-02-23 09:14:52 -0500

    feat: show chat messages with stream builder

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ChatMessages extends StatelessWidget {
   const ChatMessages({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('No messages found.'),
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('createdAt')
+          .snapshots(),
+      builder: ((context, chatSnapshot) {
+        if (chatSnapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (chatSnapshot.hasError) {
+          return const Center(
+            child: Text('There was an error. Try again later'),
+          );
+        }
+
+        if (!chatSnapshot.hasData || chatSnapshot.data!.docs.isEmpty) {
+          return const Center(
+            child: Text('No messages found.'),
+          );
+        }
+
+        final loadedMessages = chatSnapshot.data!.docs;
+
+        return ListView.builder(
+          itemCount: loadedMessages.length,
+          itemBuilder: (context, index) {
+            return Text(
+              loadedMessages[index].data()['text'],
+            );
+          },
+        );
+      }),
     );
   }
 }
